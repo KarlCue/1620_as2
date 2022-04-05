a4bac64ca2319fd26748b64490d54978bb31e684
Karl <kacue38@gmail.com>
2022-04-05 15:45:00 -0700

    able to add text but its multiple

--- src/app.js
+++ src/app.js
@@ -5,3 +5,24 @@ const notes = [
     id: 1 
   }
 ]
+
+const addTextBtn = document.querySelector('.create-note-area')
+const saveTextBtn = document.querySelector('.saveButton')
+const deleteTextBtn = document.querySelector('.deleteButton')
+
+
+newText = `         </div>
+<div id="buttons">
+    <textarea id="story" title="story"
+    rows="5" cols="40" placeholder="Enter Text Here"></textarea>
+<div id="optionbutton">
+    <button class="saveButton">save</button>
+    <button class="deleteButton">delete</button>
+</div>  `
+
+function addNewText(){
+  const newTextArea = document.querySelector(".write-note-area")
+  newTextArea.insertAdjacentHTML('afterbegin', newText)
+}
+
+addTextBtn.addEventListener('click',addNewText)
