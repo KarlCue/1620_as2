93b294b807ae108fa3325fd89d7e290318c11d30
Karl <kacue38@gmail.com>
2022-04-08 17:44:03 -0700

    able to add the title of the note on the side bar but the id doesnt increase

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const addTextBtn = document.querySelector('.icons')
 newText = ` 
 <div id="text">
     <textarea id="story" title="story"
-    rows="5" cols="40" placeholder="Enter Text Here"></textarea>
+    rows="5" cols="40" placeholder="First line is the title"></textarea>
 <div id="optionbutton">
     <button class="saveButton">save</button>
     <button class="deleteButton">delete</button>
@@ -36,7 +36,41 @@ function addNewText(){
     }
   
     deleteTextBtn.addEventListener('click', deleteText)
-}
+    
+    //Save Text
 
+    const saveTextBtn = document.querySelector('.saveButton')
+
+    function newPad(noteBody){
+      const titles = noteBody.split('\n')[0]
+      const body = noteBody.split('\n')[1]
+      test = {title:titles,noteBody:body,id:2}
+      notes.push(test)
+      template = ` <ul>${titles}</ul>`
+      console.log(notes)
+      return template
+    }
+
+    function displayPad(note){
+      const noteDisplayArea = document.querySelector('.notes-list')
+      noteDisplayArea.insertAdjacentHTML('afterbegin', note)
+    }
+
+    function takeNote(){
+      const notePad = document.querySelector('#story')
+      const note = notePad.value
+      return note
+    }
+
+    function assemblePad(){
+      const noteText = takeNote()
+      const note = newPad(noteText)
+      displayPad(note)
+      deleteText()
+    }
+
+    saveTextBtn.addEventListener('click',assemblePad)
+
+}
 addTextBtn.addEventListener('click',addNewText)
 
